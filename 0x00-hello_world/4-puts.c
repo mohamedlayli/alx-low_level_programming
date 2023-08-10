d2e5ce3d8ba6c3b1349fbe8541fd8ed0e1c1f7d6
Mohamed Layli <laylimohmd@gmail.com>
2023-08-10 10:21:47 -0700

    dfg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
-int main (void)
+
+/**
+ * main - Start Point
+ *
+ * Description : The programe prints put function
+ *
+ * return: 0 Successs value
+ */ 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
