f1d8b1abe4788375c3ea7414d3795fa409bae4b8
Mohamed Layli <laylimohmd@gmail.com>
2023-08-10 16:59:29 -0700

    sdfsdfsdfs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,17 @@
-#!/bin/bash
-gcc 
+#include <unistd.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: print a qoute using write function
+ * 	write(int fd.const void *buf.size_t count);
+ *
+ * Return: 1 (not success);
+ */
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\";
+	write(1,quo,59);
+	return (1);
+}
