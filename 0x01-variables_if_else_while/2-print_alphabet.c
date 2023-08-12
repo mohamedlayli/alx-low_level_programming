32706133adf2e86a1861e9fbdf16cf239fac63fc
Mohamed Layli <laylimohmd@gmail.com>
2023-08-12 07:05:15 -0700

    b

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point: prints the last digit of the random
+*
+* Description: number stored in the variable n
+*
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+
+{
+
+	char ch;
+
+
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	{
+
+		putchar(ch);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
