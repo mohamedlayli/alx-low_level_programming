5364079d7461056891ab1eef26bf1ee476a4dd24
mohamedlayli <laylimohmd@gmail.com>
2023-11-06 08:30:42 -0800

    New Pointer Project

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *  * _memset - fills memory with a constant byte
+ *   * @s : pointed to by s with the constant byte b
+ *    * @b : constant byte
+ *     * @n : size
+ *      * Return: s
+ *       */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+		unsigned int i;
+
+			i = 0;
+				while (i < n)
+						{
+									s[i] = b;
+											i++;
+												}
+
+				return (s);
+}
+
