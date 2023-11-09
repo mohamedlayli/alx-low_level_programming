e0cb6a3804a3d114e747b191dcc052369b014308
mohamedlayli <laylimohmd@gmail.com>
2023-11-09 11:05:45 -0800

    gfg

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,45 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * print_number - prints an integer;
- * @n: integer to be printed;
- */
-void print_number(int n)
+
+
+int main(void)
+
 {
-	unsigned int n1;
 
-	if (n < 0)
-	{
-		n1 = -n;
-		_putchar('-');
-	} else
-	{
-		n1 = n;
-	}
+  int n;
 
-	if (n1 / 10)
-		print_number(n1 / 10);
+  int a[5];
 
-	_putchar((n1 % 10) + '0');
+  int *p;
+
+
+
+  a[2] = 1024;
+
+  p = &n;
+
+  /*
+
+   * write your line of code here...
+
+   * Remember:
+
+   * - you are not allowed to use a
+
+   * - you are not allowed to modify p
+
+   * - only one statement
+
+   * - you are not allowed to code anything else than this line of code
+
+   */
+
+  p[5] = 98;
+
+  /* ...so that this prints 98\n */
+
+  printf("a[2] = %d\n", a[2]);
+
+  return (0);
+
 }
