2ef700b0746bc6a6291d2706aff89e8432655967
mohamedlayli <laylimohmd@gmail.com>
2023-11-09 13:34:36 -0800

    gfgfg

---
+++ puts.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+
+  * _puts - Prints a string
+
+  * @str: The string to print
+
+  *
+
+  * Return: void
+
+  */
+
+
+
+void _puts(char *s)
+
+{
+
+	printf("%s\n", s);
+
+}
+
