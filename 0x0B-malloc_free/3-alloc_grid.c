af1259c3e0b12555140abc98e8e0f03698da1cdc
mohammed_layli <mmlayli1990@gmail.com>
2024-01-15 15:18:32 -0800

    :gg:

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -6,34 +6,35 @@
  * @height : int
  * Return: dest
  */
- 
+
 int **alloc_grid(int width, int height)
 {
 	int **tab, i, j;
 
 	tab = malloc(sizeof(*tab) * height);
-	
-	if(width <= 0 || height <= 0 || tab == 0)
+
+	if (width <= 0 || height <= 0 || tab == 0)
 	{
-			return (NULL);
+		return (NULL);
 	}
 	else
 	{
-			for (i = 0; i < height; i++)
+		for (i = 0; i < height; i++)
+		{
+			tab[i] = malloc(sizeof(**tab) * width);
+			if (tab[i] == 0)
 			{
-					tab[i] = malloc(sizeof(**tab) * width);
-					if (tab[i] == 0)
-					{
-							while (i--)
-									free(tab[i]);
-							free(tab);
-							return (NULL);
-					}
-
-					for (j = 0; j < width; j++)	
-							tab[i][j] = 0;
+				while (i--)
+					free(tab[i]);
+				free(tab);
+				return (NULL);
 			}
+
+			for (j = 0; j < width; j++)
+				tab[i][j] = 0;
+		}
 	}
-	
+
 	return (tab);
 }
+
