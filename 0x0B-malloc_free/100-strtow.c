4d5118d136eef9c83872fffd2c0c8366a7c811e6
mohammed_layli <mmlayli1990@gmail.com>
2024-01-15 14:57:17 -0800

    hello

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * strtow - splits a string into words
+ * @strtow : ponter
+ * @str : value
+ *
+ * Return: string
+ */
+char **strtow(char *str)
+
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * free_grid - Frees grid
+ *
+ * @grid: grid of memory
+ * @height: int
+ *
+ * Return: void.
+ */
+ 
+void free_grid(int **grid, int height)
+{
+		int i = 0;
+
+		for (; i < height; i++)
+				free(grid[i]);
+		free(grid);
+}
